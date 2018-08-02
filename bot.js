d4f25b5c2cea1a924f48b9ee16bcae11a3216476
Multivax <toastmetboter@gmail.com>
2018-08-03 01:29:03 +0200

    add important functionality

--- bot.js
+++ bot.js
@@ -1,11 +1,35 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
 const token = process.argv[2];
 
+function isMessageIllegal(message) {
+		if (message.content.includes("@here"))
+				return true;
+		if (message.mentions.users && message.mentions.users.size > 0)
+				return true;
+		if (message.mentions.channels && message.mentions.channels.size > 0)
+				return true;
+		if (message.mentions.roles && message.mentions.roles.size > 0)
+				return true
+		return false;
+}
+
 client.on('ready', () => {
 		console.log("logged in");
+});
+
+client.on('message', message => {
+		if (message.author.bot) return;
+
+		if (isMessageIllegal(message)) {
+				message.member.ban({
+						reason:message.content
+				});
+				message.delete();
+		}
+
+		message.channel.send("@everyone just got banned for breaking the rules.");
 });
 
 client.on('guildMemberAdd', guildMember => {
