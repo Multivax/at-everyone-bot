50aabaa415cd59fda428fd06dc29772ee6565bcb
Multivax <toastmetboter@gmail.com>
2018-08-06 23:38:25 +0200

    add eggplant bot

---
+++ eggplant.js
@@ -0,0 +1,16 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const token = process.argv[2];
+
+client.on('ready', () => {
+	console.log("logged in");
+});
+
+client.on('message', message => {
+	if (message.content.includes("egg")) {
+		message.react("🍆");
+	}
+});
+
+client.login(token);
