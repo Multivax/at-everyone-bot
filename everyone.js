81d0e6168ee5c50246c0b031e903953799841a3a
Multivax <toastmetboter@gmail.com>
2018-08-07 15:52:59 +0200

    fix formatting for everyone boi

--- everyone.js
+++ everyone.js
@@ -4,41 +4,41 @@ const client = new Discord.Client();
 const token = process.argv[2];
 
 function isMessageIllegal(message) {
-		if (message.content.includes("@here"))
-				return true;
-		if (message.mentions.users && message.mentions.users.size > 0)
-				return true;
-		if (message.mentions.channels && message.mentions.channels.size > 0)
-				return true;
-		if (message.mentions.roles && message.mentions.roles.size > 0)
-				return true
-		return false;
+	if (message.content.includes("@here"))
+		return true;
+	if (message.mentions.users && message.mentions.users.size > 0)
+		return true;
+	if (message.mentions.channels && message.mentions.channels.size > 0)
+		return true;
+	if (message.mentions.roles && message.mentions.roles.size > 0)
+		return true
+	return false;
 }
 
 function fixInfraction(message) {
-		message.member.ban({
-				reason:message.content.substring(0, 511)
-		});
-		message.delete();
-		message.channel.send("@everyone just got banned for breaking the rules.");
+	message.member.ban({
+		reason: message.content.substring(0, 511)
+	});
+	message.delete();
+	message.channel.send("@everyone just got banned for breaking the rules.");
 }
 
 client.on('ready', () => {
-		console.log("logged in");
+	console.log("logged in");
 });
 
 client.on('message', message => {
-		if (message.author.bot) return;
-		if (isMessageIllegal(message)) fixInfraction(message);
+	if (message.author.bot) return;
+	if (isMessageIllegal(message)) fixInfraction(message);
 });
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
-		if (newMessage.author.bot) return;
-		if (isMessageIllegal(newMessage)) fixInfraction(newMessage);
+	if (newMessage.author.bot) return;
+	if (isMessageIllegal(newMessage)) fixInfraction(newMessage);
 });
 
 client.on('guildMemberAdd', guildMember => {
-		guildMember.setNickname("everyone");
+	guildMember.setNickname("everyone");
 });
 
 client.login(token);
